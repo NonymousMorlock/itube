98dc1fb05268cc2fe6128b3252eb6fff8fd44e43
NonymousMorlock <agamspaul@gmail.com>
2025-12-23 21:04:00 +0000

    ✨ feat(core): Add common typedefs for `DataMap`, `ResultFuture`, `ResultStream`, and `AuthTokens`

---
+++ lib/core/typedefs.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:itube/core/errors/failures.dart';
+
+typedef DataMap = Map<String, dynamic>;
+typedef ResultFuture<T> = Future<Either<Failure, T>>;
+typedef ResultStream<T> = Stream<Either<Failure, T>>;
+typedef AuthTokens = ({String accessToken, String refreshToken});
