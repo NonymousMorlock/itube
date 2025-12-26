e04609ac89c0ee47995edef3488f9062cdc97e51
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 17:53:42 +0000

    ✅ build(development): integrate `device_preview` in development entry point

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,13 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:itube/app/app.dart';
 import 'package:itube/bootstrap.dart';
 
 Future<void> main() async {
-  await bootstrap(() => const App());
+  await bootstrap(
+    () => DevicePreview(
+      enabled: !kIsWasm && !kIsWeb,
+      builder: (_) => const App(),
+    ),
+  );
 }
