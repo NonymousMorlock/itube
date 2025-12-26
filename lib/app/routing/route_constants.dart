b78085bb3da0ca0bf7bd672617d0fc4403214836
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 17:38:18 +0000

    ✨ feat(routing): Add `RouteConstants` class for route management

---
+++ lib/app/routing/route_constants.dart
@@ -0,0 +1,11 @@
+
+
+sealed class RouteConstants {
+  const RouteConstants();
+
+  static const initialRoute = '/';
+
+  static const authRoutes = <String>[];
+
+  static const publicRoutes = <String>[];
+}
