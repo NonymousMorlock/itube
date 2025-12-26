21b4386a3a11436ab8f1ff2ded8ed345a6959d7e
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 00:01:06 +0000

    ✨ feat(core): Add `mediaQuery` and `screenSize` extensions to `BuildContext`

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 extension ContextExtensions on BuildContext {
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  Size get screenSize => MediaQuery.sizeOf(this);
+
   ThemeData get theme => Theme.of(this);
 
   TextTheme get textTheme => theme.textTheme;
