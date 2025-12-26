3eac8e5aa21392272a4583b2d85402e9c150d14a
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 00:11:03 +0000

    ✨ feat(auth): Add `auth.dart` barrel file to export authentication views

---
+++ lib/src/auth/auth.dart
@@ -0,0 +1,3 @@
+export 'package:itube/src/auth/presentation/views/confirm_signup_page.dart';
+export 'package:itube/src/auth/presentation/views/login_page.dart';
+export 'package:itube/src/auth/presentation/views/signup_page.dart';
