d3ce9e2c4af3889d95872ab82138d929791392b8
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 17:36:17 +0000

    ✨ feat(core): Add `AdaptiveProgressIndicator` widget

---
+++ lib/core/presentation/widgets/adaptive_progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:itube/core/extensions/context_extensions.dart';
+
+class AdaptiveProgressIndicator extends StatelessWidget {
+  const AdaptiveProgressIndicator({super.key, this.color});
+
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator.adaptive(
+      strokeWidth: 2,
+      valueColor: AlwaysStoppedAnimation(color ?? context.theme.primaryColor),
+    );
+  }
+}
