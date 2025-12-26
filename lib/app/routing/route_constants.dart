8d994218a216e2aa2d871146e9d07af9bf9d14ce
NonymousMorlock <agamspaul@gmail.com>
2025-12-26 17:43:13 +0000

    ✨ feat(routing): Add signup, login, and confirm signup routes to `authRoutes`

--- lib/app/routing/route_constants.dart
+++ lib/app/routing/route_constants.dart
@@ -1,11 +1,15 @@
-
+import 'package:itube/src/auth/auth.dart';
 
 sealed class RouteConstants {
   const RouteConstants();
 
   static const initialRoute = '/';
 
-  static const authRoutes = <String>[];
+  static const authRoutes = <String>[
+    SignupPage.path,
+    LoginPage.path,
+    ConfirmSignupPage.path,
+  ];
 
   static const publicRoutes = <String>[];
 }
