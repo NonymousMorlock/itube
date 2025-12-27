bdc244f69b12b4f1969a192f3f19ec466f3bfa8e
NonymousMorlock <agamspaul@gmail.com>
2025-12-27 17:09:28 +0000

    ✅ build(bootstrap): Use `debugPrint` for logging on web and wasm platforms in debug mode

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:itube/app/di/injection_container.dart';
@@ -9,15 +10,25 @@ import 'package:itube/app/di/injection_container.dart';
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
 
+  bool get usePrint => (kIsWeb || kIsWasm) && kDebugMode;
+
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
+    if (usePrint) {
+      debugPrint('onChange(${bloc.runtimeType}, $change)');
+    } else {
+      log('onChange(${bloc.runtimeType}, $change)');
+    }
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    if (usePrint) {
+      debugPrint('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    } else {
+      log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    }
     super.onError(bloc, error, stackTrace);
   }
 }
@@ -25,7 +36,12 @@ class AppBlocObserver extends BlocObserver {
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   WidgetsFlutterBinding.ensureInitialized();
   FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
+    if ((kIsWeb || kIsWasm) && kDebugMode) {
+      debugPrint(details.exceptionAsString());
+      debugPrintStack(stackTrace: details.stack);
+    } else {
+      log(details.exceptionAsString(), stackTrace: details.stack);
+    }
   };
 
   Bloc.observer = const AppBlocObserver();
